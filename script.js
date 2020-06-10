f118a98ebfe2b41fe01f8b8d263c768f88c449e9
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-06-10 17:13:33 -0500

    Fix equal operation to correctly display multiple numbers in one operate

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let calculate = {
         return this.add(a, b);
       case "-":
         return this.subtract(a, b);
-      case "*":
+      case "X":
         return this.multiply(a, b);
       case "/":
         return this.divide(a, b);
@@ -33,26 +33,44 @@ let operates = {
   operate2: "",
   operator: "",
   firstOperate: function(e) {
+    if(this.operate1 === "") {
+      display.textContent = "";
+    };
     this.operate1 += e.target.textContent;
-    display.textContent = this.operate1;
+    display.textContent += e.target.textContent;
     console.log(`operate1: ${this.operate1}`)
   },
   secondOperate: function(e) {
     this.operate2 += e.target.textContent;
-    display.textContent = this.operate2;
+    display.textContent += e.target.textContent;
     console.log(`operate2: ${this.operate2}`);
   },
   setOperator: function(e) {
     if(e.target.classList.contains('operator')) {
       this.operator = e.target.textContent;
-      display.textContent = this.operator;
+      display.textContent += ` ${this.operator} `;
       console.log(`operator: ${this.operator}`);
     }
+  },
+  resetOperates: function() {
+    operates.operate1 = "";
+    operates.operate2 = "";
+    operates.operator = "";
+  }
+}
+
+let deleteNum = {
+  del: function(e) {
+
+  },
+  clear: function(e) {
+
   }
 }
 
 calculator.addEventListener('click', e => {
   if(e.target.classList.contains('btn')) {
+    if(display.textContent === "0") { display.textContent = "" };
     if(e.target.classList.contains('num') && operates.operator !== "") {
       operates.secondOperate(e);
     } else if(e.target.classList.contains('num') && operates.operator === "") {
@@ -62,6 +80,7 @@ calculator.addEventListener('click', e => {
     }
     if(e.target.textContent === "=") {
       display.textContent = calculate.operate(operates.operator, +(operates.operate1), +operates.operate2);
+      operates.resetOperates();
     }
   }
 });
