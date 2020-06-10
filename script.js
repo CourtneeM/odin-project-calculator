48f7708c1fd80ab4be82f7a926a25e02777b848d
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-06-10 10:12:26 -0500

    Add operate function that accepts operator and runs associated function

--- script.js
+++ script.js
@@ -10,5 +10,18 @@ let calculate = {
   },
   divide: function(a, b) {
     return a / b;
+  },
+  operate: function(operator, a, b) {
+    switch(operator) {
+      case "+":
+        return this.add(a, b);
+      case "-":
+        return this.subtract(a, b);
+      case "*":
+        return this.multiply(a, b);
+      case "/":
+        return this.divide(a, b);
+    }
   }
 }
+
